65212d443b1590a5403905132f904ed8b7841d5c
Foo-x <www.sand.x@gmail.com>
2020-02-01 21:57:47 +0900

    Add structure

---
+++ cmd/solver/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Foo-x/sudoku/internal/app/solver"
+)
+
+func main() {
+	fmt.Println(structure.Peers["C2"])
+}
---
+++ internal/app/solver/structure.go
@@ -0,0 +1,75 @@
+package structure
+
+import (
+	s "strings"
+)
+
+const raws = "ABCDEFGHI"
+const columns = "123456789"
+const blockSize = 3
+
+var Squares []string
+var Units [][]string
+var UnitMap map[string][][]string
+var Peers map[string]map[string]struct{}
+
+func init() {
+	Squares = cross(s.Split(raws, ""), s.Split(columns, ""))
+
+	for _, r := range raws {
+		unit := filter(func(v string) bool { return s.HasPrefix(v, string([]rune{r})) }, Squares)
+		Units = append(Units, unit)
+	}
+	for _, c := range columns {
+		unit := filter(func(v string) bool { return s.HasSuffix(v, string([]rune{c})) }, Squares)
+		Units = append(Units, unit)
+	}
+	for i := 0; i < len(raws); i += blockSize {
+		for j := 0; j < len(columns); j += blockSize {
+			Units = append(Units, cross(s.Split(raws, "")[i:i+blockSize], s.Split(columns, "")[j:j+blockSize]))
+		}
+	}
+
+	UnitMap = map[string][][]string{}
+	for _, s := range Squares {
+		for _, unit := range Units {
+			for _, label := range unit {
+				if label == s {
+					UnitMap[label] = append(UnitMap[label], unit)
+				}
+			}
+		}
+	}
+
+	Peers = map[string]map[string]struct{}{}
+	for _, s := range Squares {
+		peer := map[string]struct{}{}
+		for _, units4Key := range UnitMap[s] {
+			for _, label := range units4Key {
+				peer[label] = struct{}{}
+			}
+		}
+		delete(peer, s)
+		Peers[s] = peer
+	}
+}
+
+func cross(sliceA, sliceB []string) []string {
+	var crossed []string
+	for _, a := range sliceA {
+		for _, b := range sliceB {
+			crossed = append(crossed, a+b)
+		}
+	}
+	return crossed
+}
+
+func filter(f func(v string) bool, s []string) []string {
+	var filtered []string
+	for _, x := range s {
+		if f(x) {
+			filtered = append(filtered, x)
+		}
+	}
+	return filtered
+}
